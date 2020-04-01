832c53b4d25c2ccee06cdac3b6944c1bebbf39d0
xyh <1324833746@qq.com>
2020-04-01 15:23:10 +0800

    0401

---
+++ glxt1.0/src/components/mybooks/mybooks.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>mybooks</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ glxt1.0/src/components/myevents/myevents.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>myevents</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
